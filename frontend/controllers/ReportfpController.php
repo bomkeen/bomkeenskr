a9c14c9b06b38b1e1790b562fa3965b3ff5d95be
iamaliz <iamaliz@bomkeen-lenovo>
2016-03-23 14:18:29 +0700

    add to git

--- frontend/controllers/ReportfpController.php
+++ frontend/controllers/ReportfpController.php
@@ -1,63 +1,72 @@
 <?php
 
 namespace frontend\controllers;
-use yii;
 
+use yii;
 ?>
-<?php 
-class ReportfpController extends \yii\web\Controller
-{
-    public function actionIndex()
-    {
+<?php
+
+class ReportfpController extends \yii\web\Controller {
+
+    public function actionIndex() {
         return $this->render('index');
     }
-    
+
     public function actionVaccine() {
-        $date1=  date('Y-m-d');
-        $date2=  date('Y-m-d');
+        $date1 = date('Y-m-d');
+        $date2 = date('Y-m-d');
         if (Yii::$app->request->isPost) {
             $request = Yii::$app->request;
             $date1 = $request->post('date1');
             $date2 = $request->post('date2');
-       $all_sql="SELECT
-SUM(CASE WHEN (ov.vsttime BETWEEN '16:30:01' AND '24:00:00' or ov.vsttime BETWEEN '00:00:00' AND '08:29:59') THEN 1 ELSE 0 END) AS outtime
-,SUM(CASE WHEN ov.vsttime BETWEEN '08:30:00' AND '16:30:00' THEN 1 ELSE 0 END) AS intime
-,COUNT(ov.vn) AS cc,COUNT(DISTINCT ov.hn) AS dissum
-FROM	ovst AS ov WHERE ov.vstdate BETWEEN '$date1' AND '$date2'";
-       $opd_all="SELECT SUM(CASE WHEN (ov.vsttime BETWEEN '16:30:01' AND '24:00:00' or ov.vsttime BETWEEN '00:00:00' AND '08:29:59') THEN 1 ELSE 0 END) AS outtime
-,SUM(CASE WHEN ov.vsttime BETWEEN '08:30:00' AND '16:30:00' THEN 1 ELSE 0 END) AS intime
-,COUNT(ov.vn) AS cc,COUNT(DISTINCT ov.hn) AS dissum
-FROM	ovst AS ov WHERE ov.vstdate BETWEEN '$date1' and '$date2'
-AND ov.vn not in (SELECT vn from er_regist WHERE vstdate BETWEEN '$date1' and '$date2')";
-       $er_sql="SELECT SUM(CASE WHEN (ov.vsttime BETWEEN '16:30:01' AND '24:00:00' or ov.vsttime BETWEEN '00:00:00' AND '08:29:59') THEN 1 ELSE 0 END) AS outtime
-,SUM(CASE WHEN ov.vsttime BETWEEN '08:30:00' AND '16:30:00' THEN 1 ELSE 0 END) AS intime
-,COUNT(ov.vn) AS cc,COUNT(DISTINCT ov.hn) AS dissum
-FROM	ovst AS ov INNER JOIN er_regist AS er ON er.vn=ov.vn
-WHERE ov.vstdate BETWEEN '$date1' and '$date2'";
-       $dent_sql="SELECT COUNT(dt.vn) AS cc,COUNT(DISTINCT dt.hn) AS dissum
-FROM dtmain AS dt WHERE dt.vstdate BETWEEN '$date1' AND '$date2'";
-       $anc_sql="SELECT COUNT(s.vn) as cc
-,COUNT(DISTINCT s.person_anc_id) as dissum FROM person_anc_service as s
-WHERE s.anc_service_date BETWEEN '$date1' and '$date2'";
-        $rsall = \Yii::$app->hosxpslave->createCommand("$all_sql")->queryAll();
-        $rsopd_all = \Yii::$app->hosxpslave->createCommand("$opd_all")->queryAll();
-        $er = \Yii::$app->hosxpslave->createCommand("$er_sql")->queryAll();
-        $rsdent = \Yii::$app->hosxpslave->createCommand("$dent_sql")->queryAll();
-        $rsanc = \Yii::$app->hosxpslave->createCommand("$anc_sql")->queryAll();
-        return $this->render('vaccine',[
-            'date1'=>$date1,
-            'date2'=>$date2,
-            'rsall'=>$rsall,
-            'rsopd_all'=>$rsopd_all,
-            'er'=>$er,
-            'rsdent'=>$rsdent,
-            'rsanc'=>$rsanc,
-        ]);
+            $export = $request->post('export');
+            $vac_in_sql = "SELECT COUNT(*) as n,pv.vaccine_name as name FROM person_vaccine_list AS pvl 
+JOIN person_vaccine pv ON pvl.person_vaccine_id=pv.person_vaccine_id
+WHERE pvl.vaccine_date BETWEEN $date1 and $date2
+GROUP BY pvl.person_vaccine_id ";
+            $vac_out_sql = "SELECT COUNT(*) as n,vaccine_name FROM ovst_vaccine opv
+JOIN person_vaccine pv ON opv.person_vaccine_id=pv.person_vaccine_id
+JOIN ovst ov ON opv.vn=ov.vn
+WHERE ov.vstdate BETWEEN '2015-10-01' and '2015-10-31'
+GROUP BY opv.person_vaccine_id";
+
+            $vac_in = \Yii::$app->hosxpslave->createCommand("$vac_in_sql")->queryAll();
+            $vac_out = \Yii::$app->hosxpslave->createCommand("$vac_out_sql")->queryAll();
+            ///////////////////   PDF   ////////////////////////////////////////////            
+//
+            if ($export == 'pdf') {
+                $content = $this->renderPartial('vaccine', [
+                    'date1' => $date1,
+                    'date2' => $date2,
+                    'vac_in' => $vac_in,
+                    'vac_out' => $vac_out,
+                    'export' => $export,
+                ]);
+                $pdf = new Pdf([
+                    'mode' => Pdf::MODE_CORE,
+                    'content' => $content,
+                    'options' => ['title' => 'สรุปข้อมูลกาให้บริการ vaccine'],
+                    'methods' => [
+                        'SetHeader' => ['สรุปข้อมูลกาให้บริการ vaccine'],
+                        'SetFooter' => ['{PAGENO}'],
+                    ]
+                ]);
+                return $pdf->render();
+            }
+            //////////////////////////////////////////////////       
+            else {
+                return $this->render('vaccine', [
+                            'date1' => $date1,
+                            'date2' => $date2,
+                            'vac_in' => $vac_in,
+                            'vac_out' => $vac_out,
+                            'export' => $export,
+                ]);
+            }
         }
-        /////////////// ก่อน post///////////////
-        return $this->render('vaccine',[
-            'date1'=>$date1,
-            'date2'=>$date2,
+        return $this->render('vaccine', [
+                    'date1' => $date1,
+                    'date2' => $date2,
         ]);
     }
 
---
+++ frontend/views/reportfp/vaccine.php
@@ -0,0 +1,102 @@
+<?php
+
+use kartik\grid\GridView;
+use yii\helpers\Html;
+
+$this->title = 'สรุปข้อมูลกาให้บริการ vaccine';
+$this->params['breadcrumbs'][] = ['label' => 'ระบบรายงาน', 'url' => ['report/index']];
+$this->params['breadcrumbs'][] = ['label' => 'ระบบรายงาน FP', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+include_once '../../inc/thaidate.php';
+?>
+
+<div class='row'>
+    <div class="col-md-4">
+        <h4> <div class="label label-info">รายงาน <?php echo $this->title; ?></div></h4>
+    </div>
+
+    <div class="col-md-8">
+        <form class="form-inline" method="POST">
+
+            <div class="form-group">
+                <label for="date1">ระหว่าง :</label>
+                <?php
+                echo yii\jui\DatePicker::widget([
+                    'name' => 'date1',
+                    'value' => $date1,
+                    'language' => 'th',
+                    'id' => 'date1',
+                    'dateFormat' => 'yyyy-MM-dd',
+                    'options' => ['class' => 'form-control'],
+                    'clientOptions' => [
+                        'changeMonth' => true,
+                        'changeYear' => true,
+                    ],
+                ]);
+                ?>
+            </div>
+            <div class="form-group">
+                <label for="date2">ถึง:</label>
+
+                <?php
+                echo yii\jui\DatePicker::widget([
+                    'name' => 'date2',
+                    'value' => $date2,
+                    'language' => 'th',
+                    'id' => 'date2',
+                    'class' => 'form-control',
+                    'dateFormat' => 'yyyy-MM-dd',
+                    'options' => ['class' => 'form-control'],
+                    'clientOptions' => [
+                        'changeMonth' => true,
+                        'changeYear' => true,
+                    ]
+                ]);
+                ?>
+            </div>
+            <div class="form-group">
+                <label for="export">ส่งออก PDF</label>
+                <input class="form-control" type="radio" id="export" name="export" value="pdf">
+            </div>
+            <div class="form-group">
+                <button class='btn btn-danger'>ประมวลผล</button>
+            </div>
+        </form>
+    </div>
+</div>
+<br>
+<?php if (isset($vac_in)) { ?>
+    <div class="row">
+        <div class="col-md-12 col-md-offset-0">
+            <table class="table table-hover">
+                <tr class="bg-success">
+                <th><center>รายการ</center></th>
+                <th><center>ครั้ง</center></th>
+                </tr>
+                <tr>
+                    <th colspan="2"><center>Vaccine ในแผน</center></th>
+                </tr>
+                  <?php foreach ($vac_in as $vac_in_rs){ ?>
+                <tr>
+               <td><?php echo $vac_in_rs['name']; ?></td>
+                <td><center><?php echo $vac_in_rs['n']; ?></center></td>
+                </tr>
+                  <?php } ?>
+                <tr>
+                    <th colspan="2"><center>Vaccine นอกแผน</center></th>
+                </tr>
+                <?php foreach ($vac_in as $vac_in_rs){ ?>
+                <tr>
+               <td><?php echo $vac_out_rs['name']; ?></td>
+                <td><center><?php echo $vac_out_rs['n']; ?></center></td>
+                </tr>
+                  <?php } ?>
+                
+            
+            </table>
+
+        </div>
+    </div>
+<?php } ?>
+
+
